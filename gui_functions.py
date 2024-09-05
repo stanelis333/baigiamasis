93a1b6fe94e8e40ce56d46ff233fac615d848be8
Sarunas <stanelis333@gmail.com>
2024-09-05 12:47:10 +0300

    small fixes

--- gui_functions.py
+++ gui_functions.py
@@ -2,6 +2,7 @@ from data_loader import load_training_data, load_test_data, train_data_path, tes
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from model import create_model, save_model, load_model
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
 from tensorflow.keras.utils import to_categorical
 from tkinter import filedialog, messagebox, simpledialog
 import matplotlib.pyplot as plt
@@ -10,27 +11,6 @@ import numpy as np
 import threading
 import cv2
 import os 
-
-classes = { 
-    0:'Speed limit (20km/h)', 1:'Speed limit (30km/h)', 2:'Speed limit (50km/h)', 
-    3:'Speed limit (60km/h)', 4:'Speed limit (70km/h)', 5:'Speed limit (80km/h)', 
-    6:'End of speed limit (80km/h)', 7:'Speed limit (100km/h)', 8:'Speed limit (120km/h)', 
-    9:'No passing', 10:'No passing veh over 3.5 tons', 11:'Right-of-way at intersection', 
-    12:'Priority road', 13:'Yield', 14:'Stop', 15:'No vehicles', 
-    16:'Veh > 3.5 tons prohibited', 17:'No entry', 18:'General caution', 
-    19:'Dangerous curve left', 20:'Dangerous curve right', 21:'Double curve', 
-    22:'Bumpy road', 23:'Slippery road', 24:'Road narrows on the right', 
-    25:'Road work', 26:'Traffic signals', 27:'Pedestrians', 28:'Children crossing', 
-    29:'Bicycles crossing', 30:'Beware of ice/snow', 31:'Wild animals crossing', 
-    32:'End speed + passing limits', 33:'Turn right ahead', 34:'Turn left ahead', 
-    35:'Ahead only', 36:'Go straight or right', 37:'Go straight or left', 
-    38:'Keep right', 39:'Keep left', 40:'Roundabout mandatory', 
-    41:'End of no passing', 42:'End no passing veh > 3.5 tons' 
-}
-
-from sklearn.metrics import classification_report
-
-import threading
 
 def start_training():
     def run_training(epochs):
@@ -175,3 +155,20 @@ def show_data_distribution():
     except Exception as e:
         messagebox.showerror("Error", f"An error occurred while generating the graph: {e}")
 
+
+classes = { 
+    0:'Speed limit (20km/h)', 1:'Speed limit (30km/h)', 2:'Speed limit (50km/h)', 
+    3:'Speed limit (60km/h)', 4:'Speed limit (70km/h)', 5:'Speed limit (80km/h)', 
+    6:'End of speed limit (80km/h)', 7:'Speed limit (100km/h)', 8:'Speed limit (120km/h)', 
+    9:'No passing', 10:'No passing veh over 3.5 tons', 11:'Right-of-way at intersection', 
+    12:'Priority road', 13:'Yield', 14:'Stop', 15:'No vehicles', 
+    16:'Veh > 3.5 tons prohibited', 17:'No entry', 18:'General caution', 
+    19:'Dangerous curve left', 20:'Dangerous curve right', 21:'Double curve', 
+    22:'Bumpy road', 23:'Slippery road', 24:'Road narrows on the right', 
+    25:'Road work', 26:'Traffic signals', 27:'Pedestrians', 28:'Children crossing', 
+    29:'Bicycles crossing', 30:'Beware of ice/snow', 31:'Wild animals crossing', 
+    32:'End speed + passing limits', 33:'Turn right ahead', 34:'Turn left ahead', 
+    35:'Ahead only', 36:'Go straight or right', 37:'Go straight or left', 
+    38:'Keep right', 39:'Keep left', 40:'Roundabout mandatory', 
+    41:'End of no passing', 42:'End no passing veh > 3.5 tons' 
+}
